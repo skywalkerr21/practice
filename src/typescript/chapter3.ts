b1c4f937ef57fa298c5e2125a3de51e902648d86
Reia Imamura <s.r.s_yeah@ezweb.ne.jp>
2023-10-07 23:57:34 +0900

    chapter02の章末問題をhtmlのchapter02に入力

--- src/typescript/chapter3.ts
+++ src/typescript/chapter3.ts
@@ -2,3 +2,7 @@ let price: number = 120
 let input: number =200
 let change: number =input - price 
 console.log(change)
+
+const array: string[] = ['今村','友田']
+const a: string = array[0]
+console.log(a)
